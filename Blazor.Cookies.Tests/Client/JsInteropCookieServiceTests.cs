a6384ab6609a23db940eb140d2d48a9437a42e6c
Steven de Valk <steven.devalk@atos.net>
2025-05-26 20:20:12 +0200

    Added GetAsync_WithSingleCookie_ShouldReturnCookie to JsInteropCookieServiceTests;

--- Blazor.Cookies.Tests/Client/JsInteropCookieServiceTests.cs
+++ Blazor.Cookies.Tests/Client/JsInteropCookieServiceTests.cs
@@ -168,5 +168,20 @@ namespace Blazor.Cookies.Tests.Client
             Assert.NotNull(cartItemsCookie);
             Assert.Equal(cookies.First(c => c.Name == "cartItems"), cartItemsCookie);
         }
+        [Fact]
+        public async Task GetAsync_WithSingleCookie_ShouldReturnCookie()
+        {
+            List<Cookie> cookies = new List<Cookie>
+            {
+                new Cookie("sessionId", "ei34jdh")
+            };
+
+            var jsRuntime = GetMockJSRuntime(cookies);
+            JsInteropCookieService cookieService = new JsInteropCookieService(jsRuntime);
+
+            var sessionIdCookie = await cookieService.GetAsync("sessionId");
+            Assert.NotNull(sessionIdCookie);
+            Assert.Equal(cookies.First(c => c.Name == "sessionId"), sessionIdCookie);
+        }
     }
 }
