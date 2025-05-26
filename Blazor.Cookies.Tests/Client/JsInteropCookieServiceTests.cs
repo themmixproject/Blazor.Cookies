548bba0128da940d1b3b23de2ed2ece53a7fd8f3
Steven de Valk <steven.devalk@atos.net>
2025-05-26 10:21:40 +0200

    Added basic GetAllAsync_WithCookies_ReturnsIEnumerable test to JsInteropCookieServiceTests in Blazor.Cookies.Tests; Installed Moq package in Blazor.Cookies.Tests; Added project reference to Blazor.Cookies;

--- Blazor.Cookies.Tests/Client/JsInteropCookieServiceTests.cs
+++ Blazor.Cookies.Tests/Client/JsInteropCookieServiceTests.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Blazor.Cookies.Client.Services;
+using Blazor.Cookies.Interfaces;
+using Microsoft.JSInterop;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blazor.Cookies.Tests.Client
 {
-    internal class JsInteropCookieServiceTests
+    public class JsInteropCookieServiceTests
     {
+        [Fact]
+        public async Task GetAllAsync_WithCookies_ReturnsIEnumerable()
+        {
+            ICookieService cookieService = Get_GetAllAsync_WithCookies_CookieService();
+            
+            var cookies = await cookieService.GetAllAsync();
+
+            Assert.NotEmpty(cookies);
+            Assert.IsAssignableFrom<IEnumerable<Cookie>>(cookies);
+        }
+        private ICookieService Get_GetAllAsync_WithCookies_CookieService()
+        {
+            var jsRuntime = new Mock<IJSRuntime>();
+            jsRuntime.Setup(x => x.InvokeAsync<string>("eval", It.IsAny<object[]>()))
+                .ReturnsAsync("sessionId=abc123; userId=xyz789; theme=dark; cartItems=5");
+            return new JsInteropCookieService(jsRuntime.Object);
+        }
+
+
     }
 }
