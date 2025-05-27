5c4f44c7a0ae09187c57f434d299f326c5181716
Steven de Valk <steven.devalk@atos.net>
2025-05-27 16:15:30 +0200

    Added GetAllAsync_WithEmptyValueCookies_ShouldReturnCookieIEnumerable test to HttpContextCookieServiceTests;

--- Blazor.Cookies.Tests/Server/HttpContextCookieServiceTests.cs
+++ Blazor.Cookies.Tests/Server/HttpContextCookieServiceTests.cs
@@ -55,5 +55,54 @@ namespace Blazor.Cookies.Tests.Server
                 Assert.Contains($"{cookies[i].Name}={cookies[i].Value}", resultCookies);
             }
         }
+        [Fact]
+        public async Task GetAllAsync_WithEmptyValueCookies_ShouldReturnCookieIEnumerable()
+        {
+            List<Cookie> cookies = new List<Cookie>
+            {
+                new Cookie
+                {
+                    Name = "sessionId",
+                    Value = "",
+                    Expires = DateTime.UtcNow.AddDays(1),
+                },
+                new Cookie
+                {
+                    Name = "userId",
+                    Value = "",
+                    Expires = DateTime.UtcNow.AddDays(1)
+                },
+                new Cookie
+                {
+                    Name = "theme",
+                    Value = "",
+                    Expires = DateTime.UtcNow.AddDays(1)
+                },
+                new Cookie
+                {
+                    Name = "cartItems",
+                    Value = "",
+                    Expires = DateTime.UtcNow.AddDays(1)
+                }
+            };
+
+            HttpContextAccessor httpContextAccessor = new HttpContextAccessor();
+            HttpContext httpContext = new DefaultHttpContext();
+            httpContextAccessor.HttpContext = httpContext;
+            HttpContextCookieService httpContextCookieService = new HttpContextCookieService(httpContextAccessor);
+
+            foreach (Cookie cookie in cookies)
+            {
+                await httpContextCookieService.SetAsync(cookie);
+            }
+
+            var resultCookiesValues = httpContext.Response.Headers.SetCookie;
+            string resultCookies = resultCookiesValues.ToString();
+            Assert.NotEmpty(resultCookies);
+            for (int i = 0; i < cookies.Count; i++)
+            {
+                Assert.Contains($"{cookies[i].Name}={cookies[i].Value}", resultCookies);
+            }
+        }
     }
 }
