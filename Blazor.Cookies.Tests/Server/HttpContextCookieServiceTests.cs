62fcf1ce7673f41f253a227f4b6b705868125d01
Steven de Valk <steven.devalk@atos.net>
2025-05-27 16:11:36 +0200

    Added GetAllAsync_WithCookies_ShouldReturnCookieIEnumerable test to HttpContextCookieServiceTests;

--- Blazor.Cookies.Tests/Server/HttpContextCookieServiceTests.cs
+++ Blazor.Cookies.Tests/Server/HttpContextCookieServiceTests.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Blazor.Cookies.Server.Services;
+using Microsoft.AspNetCore.Http;
+using System.Net;
 
 namespace Blazor.Cookies.Tests.Server
 {
     public class HttpContextCookieServiceTests
     {
+        [Fact]
+        public async Task GetAllAsync_WithCookies_ShouldReturnCookieIEnumerable()
+        {
+            List<Cookie> cookies = new List<Cookie>
+            {
+                new Cookie
+                {
+                    Name = "sessionId",
+                    Value = "ei34jdh",
+                    Expires = DateTime.UtcNow.AddDays(1),
+                },
+                new Cookie
+                {
+                    Name = "userId",
+                    Value = "xyz789",
+                    Expires = DateTime.UtcNow.AddDays(1)
+                },
+                new Cookie
+                {
+                    Name = "theme",
+                    Value = "dark",
+                    Expires = DateTime.UtcNow.AddDays(1)
+                },
+                new Cookie
+                {
+                    Name = "cartItems",
+                    Value = "5",
+                    Expires = DateTime.UtcNow.AddDays(1)
+                }
+            };
+
+            HttpContextAccessor httpContextAccessor = new HttpContextAccessor();
+            HttpContext httpContext = new DefaultHttpContext();
+            httpContextAccessor.HttpContext = httpContext;
+            HttpContextCookieService httpContextCookieService = new HttpContextCookieService(httpContextAccessor);
+
+            foreach(Cookie cookie in cookies)
+            {
+                await httpContextCookieService.SetAsync(cookie);
+            }
+
+            var resultCookiesValues = httpContext.Response.Headers.SetCookie;
+            string resultCookies = resultCookiesValues.ToString();
+            Assert.NotEmpty(resultCookies);
+            for (int i = 0; i < cookies.Count; i++)
+            {
+                Assert.Contains($"{cookies[i].Name}={cookies[i].Value}", resultCookies);
+            }
+        }
     }
 }
