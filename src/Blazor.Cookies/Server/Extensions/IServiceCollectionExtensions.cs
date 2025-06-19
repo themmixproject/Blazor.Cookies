f76be1f174bab032b61ae7da215ef4dd431f33b6
themmixproject <steven.devalk@gmail.com>
2025-06-19 14:03:45 +0200

    Converted namespaces to file-wide instead of wrapped over classes;

--- src/Blazor.Cookies/Server/Extensions/IServiceCollectionExtensions.cs
+++ src/Blazor.Cookies/Server/Extensions/IServiceCollectionExtensions.cs
@@ -4,26 +4,24 @@ using Blazor.Cookies.Server.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Blazor.Cookies.Server.Extensions
+namespace Blazor.Cookies.Server.Extensions;
+public static class IServiceCollectionExtensions
 {
-    public static class IServiceCollectionExtensions
+    public static IServiceCollection AddCookieService(this IServiceCollection services)
     {
-        public static IServiceCollection AddCookieService(this IServiceCollection services)
+        services.AddHttpContextAccessor();
+        services.AddScoped<JsInteropCookieService>();
+        services.AddScoped<HttpContextCookieService>();
+        services.AddScoped<ICookieService>(i =>
         {
-            services.AddHttpContextAccessor();
-            services.AddScoped<JsInteropCookieService>();
-            services.AddScoped<HttpContextCookieService>();
-            services.AddScoped<ICookieService>(i =>
-            {
-                var httpContextAccessor = i.GetRequiredService<IHttpContextAccessor>();
-                var httpContext = httpContextAccessor.HttpContext;
-                var isPrerendering = (httpContext != null && !httpContext.Response.HasStarted);
+            var httpContextAccessor = i.GetRequiredService<IHttpContextAccessor>();
+            var httpContext = httpContextAccessor.HttpContext;
+            var isPrerendering = (httpContext != null && !httpContext.Response.HasStarted);
 
-                if (isPrerendering) { return i.GetRequiredService<HttpContextCookieService>(); }
-                else { return i.GetRequiredService<JsInteropCookieService>(); }
-            });
+            if (isPrerendering) { return i.GetRequiredService<HttpContextCookieService>(); }
+            else { return i.GetRequiredService<JsInteropCookieService>(); }
+        });
 
-            return services;
-        }
+        return services;
     }
 }
