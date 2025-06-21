c45cee012be4a55c5909ad4013463a567c086fe4
themmixproject <steven.devalk@gmail.com>
2025-06-21 22:53:07 +0200

    Merge 'refactor/move-IServicecollection-files-to-root-directories' into 'main':
    commit 94242304399571fe004a0f13566fc0cbfcf65bb4
    Author: themmixproject <steven.devalk@gmail.com>
    Date:   Sat Jun 21 22:51:09 2025 +0200
    
        Moved client IServiceCollectionExtensions /Client directory;
        Moved server IServiceCollectionExtensions /Server directory;

--- src/MMIX.Blazor.Cookies/Server/IServiceCollectionExtensions.cs
+++ src/MMIX.Blazor.Cookies/Server/IServiceCollectionExtensions.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using MMIX.Blazor.Cookies.Client;
 
-namespace MMIX.Blazor.Cookies.Server.Extensions;
+namespace MMIX.Blazor.Cookies.Server;
 public static class IServiceCollectionExtensions
 {
     public static IServiceCollection AddCookieService(this IServiceCollection services)
@@ -14,7 +14,7 @@ public static class IServiceCollectionExtensions
         {
             var httpContextAccessor = i.GetRequiredService<IHttpContextAccessor>();
             var httpContext = httpContextAccessor.HttpContext;
-            var isPrerendering = (httpContext != null && !httpContext.Response.HasStarted);
+            var isPrerendering = httpContext != null && !httpContext.Response.HasStarted;
 
             if (isPrerendering) { return i.GetRequiredService<HttpContextCookieService>(); }
             else { return i.GetRequiredService<JsInteropCookieService>(); }
