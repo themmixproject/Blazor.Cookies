23e8aab477064566e819421a0150b61ba8b09a89
themmixproject <steven.devalk@gmail.com>
2025-06-27 23:27:14 +0200

    Merge 'feature/add-cookie-tests' into 'main':
    commit 44303fd1ab7f4ff12105733441328c4c569137cf
    Author: themmixproject <steven.devalk@gmail.com>
    Date:   Fri Jun 27 23:26:20 2025 +0200
    
        Set build confiuration to release for MMIX.Blazor.Cookies project;
    
    commit c3009332a7d2572c00a8841934cbcf06d97dae3a
    Author: themmixproject <steven.devalk@gmail.com>
    Date:   Fri Jun 27 23:25:45 2025 +0200
    
        Set build confiuration back to debug for MMIX.Blazor.Cookies project;
    
    commit fe2131bae658f183c0e5037dcaf674ccc2b4ce47
    Author: themmixproject <steven.devalk@gmail.com>
    Date:   Fri Jun 27 23:04:29 2025 +0200
    
        Added extra assertions for more precision in ConstructCookie_WithEmptyValue_ShouldCreateCookie, ConstructCookie_WithInvalidCharacterInValue_ShouldCreateCookie in CookieTests;
    
    commit f6a4cc34d158b5d8cb216769eb9cb5628b825d44
    Author: themmixproject <steven.devalk@gmail.com>
    Date:   Fri Jun 27 22:58:54 2025 +0200
    
        Added ConstructCookie_WithInvalidCharacterInName_ShouldThrowException, ConstructCookie_WithInvalidCharacterInNameAndEmptyValue_ShouldThrowException, ConstructCookie_WithInvalidCharacterInValue_ShouldCreateCookie tests to CookieTests;
    
    commit e497d6c893da9a6501424a6f6333951e87fb1f0e
    Author: themmixproject <steven.devalk@gmail.com>
    Date:   Fri Jun 27 22:46:23 2025 +0200
    
        Added CookieTests file to assert behaviour of System.Net.Cookie class;
        Added ConstructCookie_WithEmptyNameAndValue_ShouldThrowException, ConstructCookie_WithEmptyValue_ShouldCreateCookie, ConstructCookie_WithEmptyName_ShouldThrowException test methods to CookieTests;

---
+++ tests/MMIX.Blazor.Cookies.Tests/CookieTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+
+namespace MMIX.Blazor.Cookies.Tests
+{
+    public class CookieTests
+    {
+        [Fact]
+        public void ConstructCookie_WithEmptyNameAndValue_ShouldThrowException()
+        {
+            Assert.Throws<CookieException>(() =>
+            {
+                Cookie cookie = new Cookie("", "");
+            });
+        }
+
+        [Fact]
+        public void ConstructCookie_WithEmptyValue_ShouldCreateCookie()
+        {
+            var cookie = new Cookie("name", "");
+
+            Assert.IsType<Cookie>(cookie);
+            Assert.Empty(cookie.Name);
+            Assert.NotEmpty(cookie.Value);
+        }
+
+        [Fact]
+        public void ConstructCookie_WithEmptyName_ShouldThrowException()
+        {
+            Assert.Throws<CookieException>(() =>
+            {
+                Cookie cookie = new Cookie("", "value");
+            });
+        }
+
+        [Fact]
+        public void ConstructCookie_WithInvalidCharacterInName_ShouldThrowException()
+        {
+            Assert.Throws<CookieException>(() =>
+            {
+                Cookie cookie = new Cookie("name=", "value");
+            });
+        }
+
+        [Fact]
+        public void ConstructCookie_WithInvalidCharacterInNameAndEmptyValue_ShouldThrowException()
+        {
+            Assert.Throws<CookieException>(() =>
+            {
+                Cookie cookie = new Cookie("name=", "");
+            });
+        }
+
+        [Fact]
+        public void ConstructCookie_WithInvalidCharacterInValue_ShouldCreateCookie()
+        {
+            Cookie cookie = new Cookie("name", "value=");
+
+            Assert.IsType<Cookie>(cookie);
+            Assert.NotEmpty(cookie.Name);
+            Assert.NotEmpty(cookie.Value);
+        }
+    }
+}
