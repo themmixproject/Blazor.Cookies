e497d6c893da9a6501424a6f6333951e87fb1f0e
themmixproject <steven.devalk@gmail.com>
2025-06-27 22:46:23 +0200

    Added CookieTests file to assert behaviour of System.Net.Cookie class; Added ConstructCookie_WithEmptyNameAndValue_ShouldThrowException, ConstructCookie_WithEmptyValue_ShouldCreateCookie, ConstructCookie_WithEmptyName_ShouldThrowException test methods to CookieTests;

---
+++ tests/MMIX.Blazor.Cookies.Tests/CookieTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace MMIX.Blazor.Cookies.Tests
+{
+    public class CookieTests
+    {
+        [Fact]
+        public void ConstructCookie_WithEmptyNameAndValue_ShouldThrowException()
+        {
+            Assert.Throws<CookieException>(() =>
+            {
+                Cookie cookie = new Cookie("", "");
+            });
+        }
+
+        [Fact]
+        public void ConstructCookie_WithEmptyValue_ShouldCreateCookie()
+        {
+            var cookie = new Cookie("name", "");
+            Assert.IsType<Cookie>(cookie);
+        }
+
+        [Fact]
+        public void ConstructCookie_WithEmptyName_ShouldThrowException()
+        {
+            Assert.Throws<CookieException>(() =>
+            {
+                Cookie cookie = new Cookie("", "value");
+            });
+        }
+    }
+}
