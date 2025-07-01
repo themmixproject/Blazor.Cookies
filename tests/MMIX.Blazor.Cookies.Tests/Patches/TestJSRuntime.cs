2bc0894d0f4b7d1d8515ce6f8b05d6f7146efc24
themmixproject <steven.devalk@gmail.com>
2025-07-01 23:28:22 +0200

    Added base for custom TestJSRuntime;

--- tests/MMIX.Blazor.Cookies.Tests/Patches/TestJSRuntime.cs
+++ tests/MMIX.Blazor.Cookies.Tests/Patches/TestJSRuntime.cs
@@ -4,5 +4,15 @@ namespace MMIX.Blazor.Cookies.Tests.Patches;
 
 internal class TestJSRuntime : IJSRuntime
 {
+    private Dictionary<string, string> _cookies = new Dictionary<string, string>();
 
+    public ValueTask<TValue> InvokeAsync<TValue>(string identifier, object?[]? args)
+    {
+        return ValueTask.FromResult((TValue) (object) null);
+    }
+
+    public ValueTask<TValue> InvokeAsync<TValue>(string identifier, CancellationToken cancellationToken, object?[]? args)
+    {
+        return InvokeAsync<TValue>(identifier, args);
+    }
 }
