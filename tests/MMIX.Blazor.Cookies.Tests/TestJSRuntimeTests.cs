5b61a5e11027b026dbb45aedf83b67a08e91efcd
themmixproject <steven.devalk@gmail.com>
2025-07-03 16:36:30 +0200

    Added InvokeAsync_eval_documentcookie_WithSetCookieCommand_ShouldReturnCookieString test to TestJSRuntimeTests; Fixed output string type annotations in TestJSRuntimeTests;

--- tests/MMIX.Blazor.Cookies.Tests/TestJSRuntimeTests.cs
+++ tests/MMIX.Blazor.Cookies.Tests/TestJSRuntimeTests.cs
@@ -10,10 +10,22 @@ public class TestJSRuntimeTests
     public async Task InvokeAsync_eval_documentcookie_WithNoCookies_ShouldReturnEmptyString()
     {
         IJSRuntime jsRuntime = new TestJSRuntime();
-        string cookies = await jsRuntime.InvokeAsync<String>("eval", "document.cookie");
+        string cookies = await jsRuntime.InvokeAsync<string>("eval", "document.cookie");
 
         Assert.Empty(cookies);
     }
 
-    
+    [Fact]
+    public async Task InvokeAsync_eval_documentcookie_WithSetCookieCommand_ShouldReturnCookieString()
+    {
+        IJSRuntime jsRuntime = new TestJSRuntime();
+        DateTime date = DateTime.Today;
+        string cookieExpires = date.ToUniversalTime().ToString("ddd, dd yyyy hh:mm:ss \'UTC\'zzz");
+        string command = $"myCookie=myValue; expires={cookieExpires}; samesite=lax;";
+        string cookieString = await jsRuntime.InvokeAsync<string>("eval", "document.cookie");
+
+        Assert.Equal(command, cookieString);
+    }
+
+
 }
