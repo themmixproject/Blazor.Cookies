e94ebe9c8f67bed813e25e5abec1e37a89fe8ed6
themmixproject <steven.devalk@gmail.com>
2025-07-03 15:18:50 +0200

    Added InvokeAsync_eval_documentcookie_WithNoCookies_ShouldReturnEmptyString test to TestJSRuntimeTests;

---
+++ tests/MMIX.Blazor.Cookies.Tests/TestJSRuntimeTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.JSInterop;
+using MMIX.Blazor.Cookies.Tests.Patches;
+
+namespace MMIX.Blazor.Cookies.Tests;
+
+public class TestJSRuntimeTests
+{
+    [Fact]
+    public async Task InvokeAsync_eval_documentcookie_WithNoCookies_ShouldReturnEmptyString()
+    {
+        IJSRuntime jsRuntime = new TestJSRuntime();
+        string cookies = await jsRuntime.InvokeAsync<String>("eval", "document.cookie");
+
+        Assert.Empty(cookies);
+    }
+
+    
+}
